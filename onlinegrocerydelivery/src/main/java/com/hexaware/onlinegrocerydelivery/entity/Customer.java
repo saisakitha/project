852dc2cc65703c9e74d5fd0a1123bee2d6eecf53
himakarperugu <117923119+himakarperugu@users.noreply.github.com>
2023-11-10 07:41:44 +0530

    Add files via upload
    added 3 entity and 3 dto classes

---
+++ onlinegrocerydelivery/src/main/java/com/hexaware/onlinegrocerydelivery/entity/Customer.java
@@ -0,0 +1,94 @@
+package com.hexaware.onlinegrocerydelivery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+	
+	
+	@Id
+	private int customer_id;
+	private String customer_name;
+	private String email;
+	private String phone_number;
+	private String delivery_address;
+	
+	
+	public Customer() {
+		super();
+	}
+
+
+	public Customer(int customer_id, String customer_name, String email, String phone_number, String delivery_address) {
+		super();
+		this.customer_id = customer_id;
+		this.customer_name = customer_name;
+		this.email = email;
+		this.phone_number = phone_number;
+		this.delivery_address = delivery_address;
+	}
+
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+
+	public String getCustomer_name() {
+		return customer_name;
+	}
+
+
+	public void setCustomer_name(String customer_name) {
+		this.customer_name = customer_name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+
+	public String getDelivery_address() {
+		return delivery_address;
+	}
+
+
+	public void setDelivery_address(String delivery_address) {
+		this.delivery_address = delivery_address;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [customer_id=" + customer_id + ", customer_name=" + customer_name + ", email=" + email
+				+ ", phone_number=" + phone_number + ", delivery_address=" + delivery_address + "]";
+	}
+	
+	
+	
+	
+	
+
+}
