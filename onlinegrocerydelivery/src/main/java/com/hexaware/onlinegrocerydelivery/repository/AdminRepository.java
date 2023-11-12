b3f31f705a8a9e811386a86920ec4fcfd4dbd336
saisakitha <150312213+saisakitha@users.noreply.github.com>
2023-11-12 16:00:23 +0530

    Delete onlinegrocerydelivery directory

--- onlinegrocerydelivery/src/main/java/com/hexaware/onlinegrocerydelivery/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.onlinegrocerydelivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hexaware.onlinegrocerydelivery.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
